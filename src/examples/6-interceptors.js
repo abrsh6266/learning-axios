0a67021708c6350fb0d92d93ac9961ef31e7702c
Abrham <abrsh6266@gmail.com>
2024-03-14 04:27:09 -0800

    using interceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
-
-const url = 'https://course-api.com/react-store-products';
+import { useEffect } from "react";
+import authFetch from "../axios/custom";
+const url = "https://course-api.com/react-store-products";
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = authFetch("/react-store-products");
+    } catch (error) {
+      console.log(error.response)
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
